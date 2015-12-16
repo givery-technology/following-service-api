f938ea4e78530cd99f1905bb4b39ca79a62fff7d
takayukioda <takayuki@odashi.me>
2015-12-16 20:01:53 +0900

    Update database schema

--- specifications/database/create.sql
+++ specifications/database/create.sql
@@ -6,12 +6,15 @@ CREATE TABLE IF NOT EXISTS users (
   birthday date NULL,
   company text NULL,
   location text NULL,
+  created_at text NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at text NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE (email)
 );
 
 CREATE TABLE IF NOT EXISTS followers (
   id integer PRIMARY KEY AUTOINCREMENT,
   user_id integer NOT NULL REFERENCES users(id), -- User who is followed
-  followed_by integer NOT NULL REFERENCES users(id), -- User one who following  
+  followed_by integer NOT NULL REFERENCES users(id), -- User one who following
+  created_at text NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE (user_id, followed_by)
 );
